f8a021fcb6d363344638f9bb21ea949b3709cc70
tucha <tuychin@gmail.com>
2023-12-23 01:13:53 +0300

    redirect to bot

--- bot/views.py
+++ bot/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
 
-# Create your views here.
+
+def redirect_to_bot(request):
+    return redirect('https://t.me/my_online_courses_bot')
--- online_courses/urls.py
+++ online_courses/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from bot import views
 
 urlpatterns = [
+    path('', views.redirect_to_bot),
     path('adminka/', admin.site.urls),
 ]
